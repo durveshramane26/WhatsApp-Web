e26260d81975af4059df66dada4866d0e7f044ca
durveshramane26 <durveshramane6@gmail.com>
2022-08-20 17:34:52 +0530

    DialogBox

--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Dialog } from '@mui/material';
+
+const dialogStyle = {
+    height: '96%',
+    marginTop: '12%',
+    width: '60%',
+    maxWidth: '100%',
+    maxHeight: '100%',
+    boxShadow: 'none',
+    overflow: 'none'
+}
 
 const LoginDialog = () => {
   return (
-    <div>hello from Login</div>
-  )
+    <Dialog
+        open={true}
+        PaperProps= {{ sx: dialogStyle }}
+    >
+        helo
+    </Dialog>
+
+    )   
 }
 
 export default LoginDialog
