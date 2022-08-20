4daff7e53d5f8d0944ba2cd29ffa57ce42c2474c
durveshramane26 <durveshramane6@gmail.com>
2022-08-20 17:18:53 +0530

    Initial Background

---
+++ client/src/components/Messenger.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import LoginDialog from './account/LoginDialog';
+import { AppBar, Toolbar, styled, Box} from '@mui/material';
+
+
+const Component = styled(Box)`
+  height: 100vh;
+  background: #DCDCDC;`
+
+
+const Header = styled(AppBar)`
+  height:200px;
+  background-color: #00bfa5;
+  box-shadow: none;`
+
+
+
+const Messenger = () => {
+  return (
+    <Component>
+        <Header>
+            <Toolbar>
+
+            </Toolbar>
+        </Header>
+        <LoginDialog />
+    </Component>
+  )
+}
+
+export default Messenger
---
+++ client/src/components/account/LoginDialog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginDialog = () => {
+  return (
+    <div>hello from Login</div>
+  )
+}
+
+export default LoginDialog
