aa62299e269750e32e0bda4ae53f3e66e4151951
durveshramane26 <durveshramane6@gmail.com>
2022-08-21 19:19:18 +0530

    Material UI Icons

--- client/src/components/chat/menu/Header.jsx
+++ client/src/components/chat/menu/Header.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import { useContext } from 'react'
 import { AccountContext } from "../../../context/AccountProvider";
-import { Box } from '@mui/material'
+import { Box, styled } from '@mui/material'
+
+
+const Component = styled(Box)`
+    height: 44px;
+    background: #ededed;
+    padding: 8px; 16px;
+
+`
+
+const Image = styled('img') ({
+    height: 40,
+    width: 40,
+    borderRadius: '50%'
+    
+
+})
 
 const Header = () => {
 
