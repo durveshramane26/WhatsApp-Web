ae6cb53c484b6814dfbdf16f6e7c0a6941c33062
durveshramane26 <durveshramane6@gmail.com>
2022-08-23 22:39:54 +0530

    Profile Picture Added

--- client/src/components/drawer/Profile.jsx
+++ client/src/components/drawer/Profile.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AccountContext } from '../../context/AccountProvider'
+import { Box, styled } from '@mui/material'
 
 const Profile = () => {
+
+    const { account } = useContext(AccountContext);
   return (
-    <div>Profile</div>
+    <>
+        <Box>
+            <img src={account.picture} alt="dp" />
+        </Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+    
+    </>
   )
 }
 
