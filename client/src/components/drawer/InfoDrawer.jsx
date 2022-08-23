77c246143c48af8edf12b928a3786c74ed41742d
durveshramane26 <durveshramane6@gmail.com>
2022-08-23 22:29:53 +0530

    toggle Drawer styled

--- client/src/components/drawer/InfoDrawer.jsx
+++ client/src/components/drawer/InfoDrawer.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import { Drawer } from '@mui/material' 
+import { ArrowBack } from '@mui/icons-material'
+import { Typography, Box, styled } from '@mui/material'
+
+const Header = styled(Box)`
+    background: #008069;
+    height: 107px;
+    color: #FFFFFF;
+    display: flex;
+    & > svg, & > p {
+        margin-top: auto;
+        padding: 15px;
+        font-weight: 600;
+    }`
+
+const Component = styled(Box)`
+    background: #ededed;
+    height: 85%;
+    `
+
+
+
+
+const drawerStyle = {
+    left:20,
+    top: 17,
+    height: '95%',
+    width: '30%',
+    boxShadow: 'none'
+}
+
 
 const InfoDrawer = ({ open, setOpen}) => {
-  
 
     const  handleClose = () => {
         setOpen(false);
@@ -13,9 +42,16 @@ const InfoDrawer = ({ open, setOpen}) => {
         <Drawer
             open={open}
             onClose={handleClose}
+            PaperProps={{ sx: drawerStyle }}
+            style={{ zIndex: 1500 }}
         >
-            Hello
+            <Header>
+                <ArrowBack onClick={() => setOpen(false)} />
+                <Typography>Profile</Typography>
+            </Header>
+            <Component>
 
+            </Component>
         </Drawer>
     )
 }
