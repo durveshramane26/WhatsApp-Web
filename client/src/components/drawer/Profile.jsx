c89179a63d1070f53f394d20d6f4e1473afd828d
durveshramane26 <durveshramane6@gmail.com>
2022-08-23 22:42:33 +0530

    Styled Profile Picture

--- client/src/components/drawer/Profile.jsx
+++ client/src/components/drawer/Profile.jsx
@@ -3,6 +3,19 @@ import { useContext } from 'react'
 import { AccountContext } from '../../context/AccountProvider'
 import { Box, styled } from '@mui/material'
 
+const ImageContainer = styled(Box)`
+    dipslay: flex;
+    justify-content: center;
+    `
+
+const Image = styled('img') ({
+    width: 200,
+    height: 200,
+    borderRadius:'50%',
+    padding: '25px 0'
+
+}_)
+
 const Profile = () => {
 
     const { account } = useContext(AccountContext);
