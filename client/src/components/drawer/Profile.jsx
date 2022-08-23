39e47cbacf045c1db7b6479b11afea25a1d5272e
durveshramane26 <durveshramane6@gmail.com>
2022-08-23 22:59:12 +0530

    Styled Usename Component

--- client/src/components/drawer/Profile.jsx
+++ client/src/components/drawer/Profile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useContext } from 'react'
 import { AccountContext } from '../../context/AccountProvider'
-import { Box, styled } from '@mui/material'
+import { Box, styled, Typography } from '@mui/material'
 
 const ImageContainer = styled(Box)`
     display: flex;
@@ -16,6 +16,21 @@ const Image = styled('img') ({
 
 })
 
+const BoxWrapper = styled(Box)`
+    background: #fff;
+    padding: 12px 30px 2px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0 0.08);
+    & :first-child {
+        font-size: 13px;
+        color: #009688;
+        font-weight: 200;
+    }
+    & :last-child {
+        margin: 14px 0,
+        color: #4A4A4A;
+    }
+    `
+
 const Profile = () => {
 
     const { account } = useContext(AccountContext);
@@ -24,8 +39,13 @@ const Profile = () => {
         <ImageContainer>
             <Image src={account.picture} alt="dp" />
         </ImageContainer>
-        <Box></Box>
-        <Box></Box>
+        <BoxWrapper>
+            <Typography>Your Name</Typography>
+            <Typography>{account.name}</Typography>
+        </BoxWrapper>
+        <Box>
+            <Typography>This is not your Username or pin This name will be visible to WhatsApp contacts</Typography>
+        </Box>
         <Box></Box>
     
     </>
