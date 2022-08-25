8ebc249d014fd3e7fa77bb94251e36ac93eb8e15
durveshramane26 <durveshramane6@gmail.com>
2022-08-25 13:52:08 +0530

    Updated

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -3,7 +3,7 @@ const url = 'http://localhost:8000';
   
 export const addUser = async  (data) => {
     try {
-        await axios.post(url, data);
+        await axios.post(`${url}/add`, data);
 
     }catch(error) {
         console.log("Error while addUser API", error.message)
