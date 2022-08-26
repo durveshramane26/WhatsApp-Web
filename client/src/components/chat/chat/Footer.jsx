22335c7238e99ebad5145bd79a007741f1d0d74a
durveshramane26 <durveshramane6@gmail.com>
2022-08-26 20:18:56 +0530

    Chat Components

---
+++ client/src/components/chat/chat/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import { EmojiEmotionsOutlined } from '@mui/icons-material/'
+
+const Footer = () => {
+  return (
+    <Box>
+        <EmojiEmotionsOutlined />
+    </Box>
+  )
+}
+
+export default Footer 
--- client/src/components/chat/chat/Messages.jsx
+++ client/src/components/chat/chat/Messages.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Box , styled } from '@mui/material'
+import Footer from './Footer'
 
+const Wrapper = styled(Box)`
+  background-image: url(${'https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png'});
+  background-size: 50%;
+`
+
+const Component = styled(Box)`
+  height: 80vh;
+  overflow-y: scroll:
+`
 const Messages = () => {
   return (
-    <div>this is from message</div>
+    <Wrapper>
+      <Component>
+        
+
+      </Component>
+      <Footer />
+    </Wrapper>
   )
 }
 
