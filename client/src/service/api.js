d19fa3f2036ea4123f720f3ac6f84a2934b58b9f
durveshramane26 <durveshramane6@gmail.com>
2022-08-30 23:55:41 +0530

    Api for fetching conversation

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -51,3 +51,13 @@ export const newMessage = async (data) => {
         console.log("Error while calling newMessage API", error.message);
     }
 }
+
+
+export const getMessages = async (id) => {
+    try {
+        let response = await axios.get(`${url}/message/get/${id}`)
+        return response.data;
+    }catch(error) {
+        console.log("Error while calling getMessage api", error.message);
+    }
+}
