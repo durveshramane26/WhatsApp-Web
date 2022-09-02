ef55348dd64121745d5b67bbe4d668ba5a7db949
durveshramane26 <durveshramane6@gmail.com>
2022-09-02 16:53:00 +0530

    refactor socket io

--- socket/index.js
+++ socket/index.js
@@ -1,12 +1,45 @@
-import { Server, server } from 'socket.io';
+import { Server } from 'socket.io';
 
 const io = new Server(9000, {
     cors: {
-        origin: 'http://localhost:3000'
-    }
+        origin: 'http://localhost:3000',
+    }, 
 })
 
-io.on('connect', (socket) => {
-    console.log(' user connected');
 
+let users = [];
+
+const addUser = (userData, socketId) => {
+    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+    return users.find(user => user.sub === userId);
+}
+
+io.on('connection',  (socket) => {
+    console.log('user connected')
+
+    //connect
+    socket.on("addUser", userData => {
+        addUser(userData, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    //send message
+    socket.on('sendMessage', (data) => {
+        const user = getUser(data.receiverId);
+        io.to(user.socketId).emit('getMessage', data)
+    })
+
+    //disconnect
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        removeUser(socket.id);
+        io.emit('getUsers', users);
+    })
 })
